5b172d94254883480f87b8acdf539ecdfa354218
Marcos Garcia Vega <marcosgarciavega@gmail.com>
2024-07-08 15:55:07 -0500

    create operation buttons (+,-,*,/)

--- demo.py
+++ demo.py
@@ -14,4 +14,16 @@ for x in range(3):
 
 button = Button(root, text = '0', width=20, height=2)
 button.grid(row=5, column = 1)
+
+op_count = 0
+operations = ['+','-','*','/','*3.14','%','(','**',')','**2']
+for x in range(4):
+    for y in range (3):
+        if op_count <len(operations):
+            button = Button(root, text=operations[op_count], width=10, height=2)
+            op_count += 1
+            button.grid(row = x+2, column = y+3)
+
+
+
 root.mainloop()
