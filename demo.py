25d8ec8f5746b5a031f41f4f3c7240f63b3cdc9b
Marcos Garcia Vega <marcosgarciavega@gmail.com>
2024-07-09 14:26:01 -0500

    add functional number buttons

--- demo.py
+++ demo.py
@@ -1,18 +1,27 @@
 from tkinter import *
+i = 0
 
 root = Tk()
+def get_number(num):
+    global i
+    display.insert(i,num)
+    i+=1
+
+
 display = Entry(root, width=60)
 display.grid(row=1, column=0, columnspan=6, )
 
-counter = 1
+numbers = [1,2,3,4,5,6,7,8,9]
+counter = 0
 
 for x in range(3):
     for y in range(3):
-        button = Button(root, text = counter, width=20, height=2)
+        button_text = numbers[counter]
+        button = Button(root, text = button_text, width=20, height=2, command = lambda x = button_text:get_number(x))
         counter +=1
         button.grid(row=x+2, column=y)
 
-button = Button(root, text = '0', width=20, height=2)
+button = Button(root, text = '0', width=20, height=2,command = lambda: get_number(0))
 button.grid(row=5, column = 1)
 
 op_count = 0
